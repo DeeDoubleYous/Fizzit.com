4fb9171ba2a6445fb4a4193b29b6c2a4981a45c2
mc1098 <m.cripps1@uni.brighton.ac.uk>
2020-02-11 14:47:50 +0000

    Initial web application with maven. Uses JDK 8 to support development in the labs. Mostly generated files and directories with a simple "hello world" index page which will help us test deployment.

---
+++ src/main/java/bton/ci536/fizzit/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package bton.ci536.fizzit;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
