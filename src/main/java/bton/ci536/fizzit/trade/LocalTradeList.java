5bd7cea68fac67b1c2c01faa9c4d0f534dad7d66
mc1098 <m.cripps1@uni.brighton.ac.uk>
2020-02-16 01:32:15 +0000

    Index page for entering a barcode with some validation for the barcode and a 100 item limit.

---
+++ src/main/java/bton/ci536/fizzit/trade/LocalTradeList.java
@@ -0,0 +1,60 @@
+package bton.ci536.fizzit.trade;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ * A Local list of to-be-traded items ({@link TradeItem}) by the customer. 
+ * @see TradeItem
+ * @author Max Cripps <43726912+mc1098@users.noreply.github.com>
+ */
+@Named
+@SessionScoped
+public class LocalTradeList implements Serializable{
+    
+    private String barcode;
+    private List<TradeItem> items;
+
+    /**
+     * Method is called after creation of object 
+     * this makes sure that the items list is valid.
+     */
+    @PostConstruct
+    public void init() {
+        this.items = new ArrayList<>();
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+    
+    public List<TradeItem> getItems() {
+        return items;
+    }
+    
+    /**
+     * Submit will check the current barcode variable in this instance 
+     * and will generate a new {@link TradeItem} in the list if this 
+     * barcode is accepted.
+     * 
+     * NB: currently it just simply creates a new item from the current barcode.
+     */
+    public void submit() {
+        /*
+            create a new TradeItem from barcode
+            NB: in future this should seek item from 
+            database. 
+        */ 
+        TradeItem item = new TradeItem(barcode);
+        this.items.add(item);
+        
+    }
+}
---
+++ src/main/java/bton/ci536/fizzit/validation/BarcodeValidator.java
@@ -0,0 +1,63 @@
+package bton.ci536.fizzit.validation;
+
+import bton.ci536.fizzit.trade.LocalTradeList;
+import bton.ci536.fizzit.trade.TradeItem;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Max Cripps <43726912+mc1098@users.noreply.github.com>
+ */
+@Named
+public class BarcodeValidator implements Validator, Serializable{
+    
+    //Inject the localTradeList bean to validate limit.
+    @Inject
+    private LocalTradeList localTradeList;
+    
+    @Override
+    public void validate(
+            FacesContext context, 
+            UIComponent component, 
+            Object value            //value to validate 
+    ) throws ValidatorException {
+        
+        /*
+            Check the local list of trades against limit
+            NB: limit is hardcoded here - In future should resolve by storing 
+            this in a Bean that can be configured while the application is running.
+        */
+        if(localTradeList.getItems().size() == 100) {
+            throw new ValidatorException(
+                    new FacesMessage("100 items is the maximum trade amount!"));
+        } 
+        
+        String barcode = (String) value;
+        
+        if(barcode.matches("\\d*")){ //string is all numbers
+            switch(barcode.length())
+            {
+                case 9:     //ISBN 9 (1967 - 1970) 
+                case 10:    //ISBN 10 (ISO 2108)
+                case 13:    //EAN-13 / GTIN-14 (w/o check)
+                    return; // One condition has been met so return without fault.
+            }
+        }
+        //No accepted format found so throw Exception
+        throw new ValidatorException(
+                new FacesMessage("We accept ISBN 9/10 and EAN-13 barcodes\n"
+                        + "(so it should be either 9,10 or 13 digits!)."));
+        
+    }
+    
+    
+    
+}
