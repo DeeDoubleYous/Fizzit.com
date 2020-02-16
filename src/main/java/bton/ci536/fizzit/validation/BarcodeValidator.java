73d220e1d09d086846ea377a6fbba8ca8136f722
mc1098 <m.cripps1@uni.brighton.ac.uk>
2020-02-16 16:18:03 +0000

    Moved limit validation code to TradeLimitValidator from BarcodeValidator. Unit test for new validator too.

--- src/main/java/bton/ci536/fizzit/validation/BarcodeValidator.java
+++ src/main/java/bton/ci536/fizzit/validation/BarcodeValidator.java
@@ -1,13 +1,11 @@
 package bton.ci536.fizzit.validation;
 
-import bton.ci536.fizzit.trade.LocalTradeList;
 import java.io.Serializable;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
@@ -15,37 +13,18 @@ import javax.inject.Named;
  * @author Max Cripps <43726912+mc1098@users.noreply.github.com>
  */
 @Named
-public class BarcodeValidator implements Validator, Serializable{
+public class BarcodeValidator implements Validator<String>, Serializable{
     
-    //Inject the localTradeList bean to validate limit.
-    @Inject
-    private LocalTradeList localTradeList;
-    
-    public void setLocalTradeList(LocalTradeList localTradeList) {
-        this.localTradeList = localTradeList;
-    }
     
     @Override
     public void validate(
             FacesContext context, 
             UIComponent component, 
-            Object value            //value to validate 
+            String value            //value to validate 
     ) throws ValidatorException {
         
-        /*
-            Check the local list of trades against limit
-            NB: limit is hardcoded here - In future should resolve by storing 
-            this in a Bean that can be configured while the application is running.
-        */
-        if(localTradeList.getItems().size() == 100) {
-            throw new ValidatorException(
-                    new FacesMessage("100 items is the maximum trade amount!"));
-        } 
-        
-        String barcode = (String) value;
-        
-        if(barcode.matches("\\d*")){ //string is all numbers
-            switch(barcode.length())
+        if(value.matches("\\d*")){ //string is all numbers
+            switch(value.length())
             {
                 case 9:     //ISBN 9 (1967 - 1970) 
                 case 10:    //ISBN 10 (ISO 2108)
---
+++ src/test/java/bton/ci536/fizzit/validation/TradeLimitValidatorTest.java
@@ -0,0 +1,66 @@
+package bton.ci536.fizzit.validation;
+
+import bton.ci536.fizzit.trade.LocalTradeList;
+import javax.faces.validator.ValidatorException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Max Cripps <43726912+mc1098@users.noreply.github.com>
+ */
+public class TradeLimitValidatorTest {
+    
+    private final LocalTradeList localTradeList;
+    private final TradeLimitValidator validator;
+    
+    public TradeLimitValidatorTest() {
+        this.localTradeList = new LocalTradeList();
+        this.validator = new TradeLimitValidator();
+        validator.setLocalTradeList(localTradeList);
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testValidate() {
+        System.out.println("validate");
+        
+        //Validator actually doesn't validate the form data so all params are ignored.
+        validator.validate(null, null, null); //should run without exception.
+        
+        /*
+            Fill the localTradeList of dummy items to get to 100 limit. 
+            then test.
+        */
+        for (int i = 0; i < 100; i++) {
+            localTradeList.getItems().add(null);
+        }
+        
+        try {
+            validator.validate(null, null, null);
+            fail("Expected validator to fail as list is over limit.");
+        } catch (ValidatorException e) {
+            //Exception is expected here so test will pass if thrown.
+        }
+    }
+    
+}
