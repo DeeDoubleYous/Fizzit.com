960824681623cb0d932d65cdd9fd1d9eb6554461
DA-Wishart <wishart.dylan@outlook.com>
2020-03-16 12:03:32 +0000

    an attempt to make the login control actually interact with the database

--- src/main/java/bton/ci536/fizzit/userLogin/LoginControl.java
+++ src/main/java/bton/ci536/fizzit/userLogin/LoginControl.java
@@ -2,11 +2,20 @@ package bton.ci536.fizzit.userLogin;
 
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import bton.ci536.fizzit.database.Customer;
 
 @Named("LoginControl")
 @SessionScoped
 public class LoginControl implements Serializable{
+	
+	@PersistenceContext(name="twoo")
+	EntityManager em;
 	
 	private static final long serialVersionUID = 1L;
 	private String email;
@@ -29,7 +38,18 @@ public class LoginControl implements Serializable{
 	}
 	
 	public void submit() {
-		
+		Customer c = em.find(Customer.class, email);
+		if(c == null) {
+			c = null;
+			email = null;
+			password = null;
+			FacesContext.getCurrentInstance().addMessage("form:user-login", new FacesMessage("Unfortunatly one of either you password or email was incorrect, please try again"));
+		}else if(!c.passwordMatch(password)){
+			c = null;
+			email = null;
+			password = null;
+			FacesContext.getCurrentInstance().addMessage("form:user-login", new FacesMessage("Unfortunatly one of either you password or email was incorrect, please try again"));
+		}
 	}
 
 }
