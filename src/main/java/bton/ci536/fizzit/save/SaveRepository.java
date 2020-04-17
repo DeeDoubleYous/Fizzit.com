01c9c6bb9fc7b1cebe51300b0cce85e9bf95a9d8
DA-Wishart <wishart.dylan@outlook.com>
2020-04-17 17:29:25 +0100

    Minor additions ontop of last commit

--- src/main/java/bton/ci536/fizzit/save/SaveRepository.java
+++ src/main/java/bton/ci536/fizzit/save/SaveRepository.java
@@ -1,5 +1,27 @@
 package bton.ci536.fizzit.save;
 
+import bton.ci536.fizzit.database.Customer;
+
+import java.util.List;
+
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Named
 public class SaveRepository {
+	
+	@PersistenceContext(unitName = "fizzit")
+	EntityManager em;
+	
+	public List<SavedProduct> getByCustomer(Customer cust){
+		return getByCustomerId(cust.getCustomerId());
+	}
+	
+	public List<SavedProduct> getByCustomerId(String customerId){
+		List<SavedProduct> saved = em.createNamedQuery("byCustomer", SavedProduct.class).setParameter("custId", customerId).getResultList();
+		em.getEntityManagerFactory().getCache().evictAll();
+		return saved;
+	}
 
 }
